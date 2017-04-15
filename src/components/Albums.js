4666022082297845f53f7510c5d5ba4cf82176a7
DawidMyslak <dawid.myslak@teamwork.com>
2017-04-15 16:04:06 +0200

    Page and items

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -2,25 +2,33 @@ import React, { Component, PropTypes } from 'react';
 import { UISref } from 'ui-router-react';
 
 export default class Albums extends Component {
+  constructor() {
+    super();
+    this.state = { page: 1, items: 10 };
+  }
+
   render() {
     let { albums } = this.props.resolves;
+    let { page, items } = this.state;
 
-    let rows = albums.map(album => (
-      <tr key={album.id}>
-        <td>
-          {album.id}
-        </td>
-        <td>
-          <UISref to="album" params={{ albumId: album.id }}>
-            <a>{album.title}</a>
-          </UISref>
-        </td>
-      </tr>
-    ));
+    let rows = albums
+      .filter(album => album.id > (page - 1) * items && album.id <= page * items)
+      .map(album => (
+        <tr key={album.id}>
+          <td>
+            {album.id}
+          </td>
+          <td>
+            <UISref to="album" params={{ albumId: album.id }}>
+              <a>{album.title}</a>
+            </UISref>
+          </td>
+        </tr>
+      ));
 
     return (
       <div>
-        <h3>Some albums:</h3>
+        <h3>Albums</h3>
         <table>
           <tbody>
             {rows}
